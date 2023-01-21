c6ed7c0573eda4c3c49fdc105534426739400e8e
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-01-20 23:09:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
     <head></head>
     <body>
+    $dbconn = pg_connect("postgresql://postgres:lGMzkadcIcd4VwrSzn88@containers-us-west-126.railway.app:6625/railway")
+        or die("Could not connect");
+        echo "Connected successfully";
         <h1><?php echo "this mensage nuver";?></h1>
     </body>
 </html>
