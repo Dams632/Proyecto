fb555c881f7733ed3656e5e39be58c01ecb96782
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-01-26 22:38:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
     <head></head>
     <body>
         <?php
-        $dbconn = pg_connect("postgresql://postgres:YgonU3pdXNxE32wD5Zcy@containers-us-west-63.railway.app:7471/railway")
+         $dbconn = pg_connect("postgresql://postgres:YgonU3pdXNxE32wD5Zcy@containers-us-west-63.railway.app:7471/railway")
         or die("Could not connect");
-        echo "Connected successfully";?>
+        echo "Connected successfully";
+            
+        ?>
+
         <?php 
             $resultado= pg_query($dbconn,"SELECT nombre,cod_cancha FROM canchas");
             if(!$resultado){
@@ -18,11 +21,11 @@
         
 
         <h1><?php echo "this mensage nuver";?></h1>
-        <button type="button" onclick="<?php
-            $prueba= pg_query($dbconn,"insert into ciudades(cod_ciudad,nombre) values(256,'Villavicencio')");
-            //$query = "INSERT INTO ciudades (cod_ciudad, nombre) VALUES (256, 'Villavicencio')";
-            /*if ($dbconn->query($sql) === TRUE) {
-                echo "New record created successfully";}*/
-        ?>">Click Me!</button>
+        <?php 
+            function ingreso(){
+                $prueba= pg_query($dbconn,"insert into ciudades(cod_ciudad,nombre) values(256,'Villavicencio')");
+                }
+        ?>
+        <button type="button" onclick="ingreso()">Click Me!</button>
     </body>
 </html>
