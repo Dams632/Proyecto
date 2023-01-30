a744790732239ff9817cdfa19698684222be75fe
nicolebernal <nicolemichelle120@gmail.com>
2023-01-30 07:05:42 -0500

    Front parcial 2

--- controller/main.php
+++ controller/main.php
@@ -1,22 +1,19 @@
 <?php
-  echo "Hola mundo";
   error_reporting(E_ERROR);
   $dbconn = pg_connect("postgresql://postgres:YgonU3pdXNxE32wD5Zcy@containers-us-west-63.railway.app:7471/railway")
   or die("Could not connect");
   echo "Connected successfully";
 
-  if($dbconn){
-    echo 'No se ha podido conectar con el servidor';
-  }
-  echo 'Primer corte';
-  $query = "select nombre from administradores where nombre ='admin@gmail.com'";
-  echo 'Segundo corte';
   $username=$_POST['username'];
   $password=$_POST['password'];
+  $query = ("select nombre from administradores where nombre = '".$username."'");
   $buscando=pg_query($dbconn,$query);
 
- if(pg_num_rows($buscando)>0){
+ if(pg_num_rows($buscando)==1){
     echo 'Se encontro el registro';
+ }
+ else{
+  echo 'No hay registros con esa informacion'; 
  }
 
 
