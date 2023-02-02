fb40efcb41051eb150380553cb89ebc31b43ff50
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-02-01 22:32:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html>
-    <head>Proyecto</head>
-    <title> Proyecto</title>
+    <head><title> Proyecto</title></head>
+    
     <body>
         <div>
             <table>
                 <tr>
                     hola
                 </tr>
+                <tr>
+                    adios
+                </tr>
+                <td>
+                    buenas
+                </td>
             </table>
         </div>
     </body>
