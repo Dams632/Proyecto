acd94caa22bfaeb3a0fa0941976d9e26ca893d4f
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-02-02 11:16:42 -0500

    s

--- models/canchas.model.php
+++ models/canchas.model.php
@@ -1,24 +1,24 @@
 <?php
-require_once "/home/dams/Documentos/GitHub/Proyecto/config/conexion.php";
+require_once "config/conexion.php";
 
 class Canchas extends Conectar{
     public static function mostrarCanchas(){
         try {
             $sql= "select * from  canchas";
-            //$stml= Conectar::getConnection()->prepare($sql);
-            //$stml->execute();
-            //$resultado=$stml->fetchAll();
+            $stml= Conectar::getConnection()->prepare($sql);
+            $stml->execute();
+            $resultado=$stml->fetchAll();
             echo "vas mejor";
             
             echo '<table>';
             echo '<tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr>';
-          /*foreach ($resultado as $row) {
+          foreach ($resultado as $row) {
               echo '<tr>';
               echo '<td>' . $row['cod_cancha'] . '</td>';
               echo '<td>' . $row['nombre'] . '</td>';
               echo '<td>' . $row['capacidad'] . '</td>';
               echo '</tr>';
-            }*/
+            }
             echo '</table>';
             
         } catch (PDOException $th) {
