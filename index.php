d3ea5d7ab3addb77573cc77b11315547bbf4afb8
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-02-01 22:36:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
 <html>
-    <head><title> Proyecto</title></head>
-    
-    <body>
-        <div>
-            <table>
-                <tr>
-                    hola
-                </tr>
-                <tr>
-                    adios
-                </tr>
-                <td>
-                    buenas
-                </td>
-            </table>
-        </div>
-    </body>
-
+  <head>
+    <meta charset="UTF-8">
+    <title>Inicio de sesión</title>
+  </head>
+  <body>
+    <h1>Inicio de sesión</h1>
+    <form action="/login" method="post">
+      <label for="username">Nombre de usuario:</label>
+      <input type="text" id="username" name="username" required>
+      <br><br>
+      <label for="password">Contraseña:</label>
+      <input type="password" id="password" name="password" required>
+      <br><br>
+      <input type="submit" value="Iniciar sesión">
+    </form>
+  </body>
 </html>
