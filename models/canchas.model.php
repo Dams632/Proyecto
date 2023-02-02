0ae9635a6c15d80eb0384472c1657fc05849ade4
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-02-01 23:34:10 -0500

    aa

--- models/canchas.model.php
+++ models/canchas.model.php
@@ -9,6 +9,18 @@ class Canchas extends Conectar{
             $stml->execute();
             $resultado=$stml->fetchAll();
             echo "vas mejor";
+            
+            echo '<table>';
+            echo '<tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr>';
+          foreach ($resultado as $row) {
+              echo '<tr>';
+              echo '<td>' . $row['column_1'] . '</td>';
+              echo '<td>' . $row['column_2'] . '</td>';
+              echo '<td>' . $row['column_3'] . '</td>';
+              echo '</tr>';
+            }
+            echo '</table>';
+            
         } catch (PDOException $th) {
             echo $th->getMessage();
         }
