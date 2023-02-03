96e15a26da12822913e4c3dd86045df9097a2d91
Dams632 <89787499+Dams632@users.noreply.github.com>
2023-02-02 23:53:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,18 @@
     
   </head>
   <body>
-   
-
+  <table class="table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombres</th>
+                <th>Capacidad</th>
+                <th>Opciones</th>
+              </tr>
+            </thead>
+            <tbody id="tbody"></tbody>
+          </table>
+          
+          <script src="../assets/code.js"></script>
   </body>
 </html>
