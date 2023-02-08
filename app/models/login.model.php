aae77fccf6035bb38a5ef4566eca1330aefdfedc
Dams632 <godnabo8@gmail.com>
2023-02-08 16:36:45 -0600

    Update login.model.php

--- app/models/login.model.php
+++ app/models/login.model.php
@@ -10,13 +10,17 @@ require_once "../config/conexion.php";
             $stmt->bindParam(':password', $data['password']);
             $stmt->execute();
             $resultado = $stmt->fetch();
-            print_r($resultado);
-            if ($resultado[0] == 2) {
-                header("Location: ../view/logTecnico.php");
-            } else if ($resultado[0] == 1) {
-                header("Location: ../view/admin.php");
+           // print_r($resultado);
+            if ($resultado!=NULL) {
+                if ($resultado[0] == 2) {
+                    header("Location: ../view/logTecnico.php");
+                } else if ($resultado[0] == 1) {
+                    header("Location: ../view/admin.php");
+                }
+                return true;
+            } else {
+                //print_r("El usuario no exite");
             }
-            return true;
         }
         catch(PDOException $th){
             $th->getMessage();
