80be4bbbe85ee804d0ae0fb15e97244f7ef65e7f
Dams632 <godnabo8@gmail.com>
2023-02-08 15:37:17 -0600

    Update login.model.php

--- app/models/login.model.php
+++ app/models/login.model.php
@@ -1,18 +1,29 @@
 <?php
 require_once "../config/conexion.php";
     class login extends Conectar{
-        public static function login($data){
-            $sql = "SELECT tipo FROM usuarios WHERE \"user\"=:user AND \"password\"=  :password";
+    public static function login($data)
+    {
+        try {
+            $sql = "SELECT tipo FROM usuarios WHERE \"user\"=:user AND password=  :password";
             $stmt = Conectar::getConnection()->prepare($sql);
             $stmt->bindParam(':user', $data['user']);
             $stmt->bindParam(':password', $data['password']);
             $stmt->execute();
-            if($stmt->execute()==2)
+            $resultado = $stmt->fetch();
+            print_r($resultado);
+            if ($resultado[0] == 2) {
                 header("Location: ../view/logTecnico.php");
-            else
-                header("Location: ../view/admin.php");            
-
+            } else if ($resultado[0] == 1) {
+                header("Location: ../view/admin.php");
+            }
+            return true;
         }
+        catch(PDOException $th){
+            $th->getMessage();
+    }
+
+    }
+        
     }
 
     
